5c69b1594787eaa49613d115397e6f60c24f9996
Betterslash <dory7141@gmail.com>
2022-03-08 18:06:40 +0200

    added finilization of laboratory 2

--- sem-two/numerical-calculus/laboratory-two/laboratory_2.m
+++ sem-two/numerical-calculus/laboratory-two/laboratory_2.m
@@ -137,15 +137,32 @@ pkg load econometrics;
     #prettyprint(result, cols, lines);
     
     
-    
-    
+ ## Calculus Lab 2, Ex 4, Popa Alex Ovidiu 936/1
+## Below you have a call example: 
+  x1 = 1:0.25:2.5;
+  f1 = @(x1) sqrt(5 * x1.^2 + 1);
+  
+
+function finite_diff = ex4(x, f)
+  n = length(x);
+  finite_diff = [f(x)', zeros(n, n-1)]; % append values of f(x) as a column vector at the start, one less column since we already have f
+  for k = 2:n   # loop the triangle above the secondary diagonal of the matrix
+    for i = 1:n-k+1
+      finite_diff(i,k) = finite_diff(i+1, k-1) - finite_diff(i, k-1); # recurrence formula
+    endfor
+  endfor
+  finite_diff = [x', finite_diff]; # append the values of x as a column vector 
+endfunction   
+  
+t4 = ex4(x1, f1)
+  
 % 5
-  ##  x2 = [2 4 6 8];
-##  f2 = [4 8 14 16];
-##  t5 = ex5(x2, f2)
+  x2 = [2 4 6 8];
+  f2 = [4 8 14 16];
+  
 
 function div_diff = ex5(x, f)
-  n = length(x)
+  n = length(x);
   div_diff = [f', zeros(n, n-1)]; % append values of f as a column vector at the start, one less column since we already have f
   for k = 2:n  # loop the triangle above the secondary diagonal of the matrix
     for i = 1:n-k+1
@@ -154,7 +171,8 @@ function div_diff = ex5(x, f)
   endfor
   div_diff = [x', div_diff]; # append the values of x as a column vector 
 endfunction
-    
+
+  t5 = ex5(x2, f2)  
     
     
     
